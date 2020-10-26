1f5a463fe2262a421296d051afd50226e859626a
xFluke <mikoman1124@gmail.com>
2020-10-25 20:28:58 -0400

    Player can no longer walk inside walls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public LayerMask obstacleLayer;
+
+    Animator animator;
+
     bool isMoving = false;
     Vector3 destination;
 
@@ -13,11 +17,13 @@ public class Player : MonoBehaviour
     void Start()
     {
         destination = transform.position;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (transform.position != destination) {
             isMoving = true;
         }
@@ -28,28 +34,50 @@ public class Player : MonoBehaviour
         if (isMoving) {
             transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
         }
+
+        
     }
 
     public void MoveUp() {
-        if (!isMoving) {
-            destination = transform.position + new Vector3(0.0f, 0.5f, 0.0f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.up, 0.5f, obstacleLayer);
+
+        animator.SetInteger("XDirection", 0);
+        animator.SetInteger("YDirection", 1);
+
+        if (!isMoving && !hit) {
+            destination = transform.position + new Vector3(0.0f, 0.5f, 0.0f);            
         }
     }
 
     public void MoveDown() {
-        if (!isMoving) {
-            destination = transform.position - new Vector3(0.0f, 0.5f, 0.0f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, 0.5f, obstacleLayer);
+
+        animator.SetInteger("XDirection", 0);
+        animator.SetInteger("YDirection", -1);
+
+        if (!isMoving && !hit) {
+            destination = transform.position - new Vector3(0.0f, 0.5f, 0.0f);       
         }
     }
 
     public void MoveLeft() {
-        if (!isMoving) {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.left, 0.5f, obstacleLayer);
+
+        animator.SetInteger("XDirection", -1);
+        animator.SetInteger("YDirection", 0);
+
+        if (!isMoving && !hit) {
             destination = transform.position - new Vector3(0.5f, 0.0f, 0.0f);
         }
     }
 
     public void MoveRight() {
-        if (!isMoving) {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.right, 0.5f, obstacleLayer);
+
+        animator.SetInteger("XDirection", 1);
+        animator.SetInteger("YDirection", 0);
+
+        if (!isMoving && !hit) {
             destination = transform.position + new Vector3(0.5f, 0.0f, 0.0f);
         }
     }
