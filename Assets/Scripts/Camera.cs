7c72167e2af64f5f9850f025b3a182ea8de1d68a
xFluke <mikoman1124@gmail.com>
2020-10-25 20:50:38 -0400

    Camera now follows player

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    float maxY = 3f;
+    float minY = -3.5f;
+
+    float maxX = 2f;
+    float minX = -1.8f;
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = player.transform.position;
+        temp.x = Mathf.Clamp(temp.x, minX, maxX);
+        temp.y = Mathf.Clamp(temp.y, minY, maxY);
+        temp.z = -10f;
+        transform.position = temp;
+    }
+}
